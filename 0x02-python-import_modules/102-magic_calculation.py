42f806f685f95000714ec9cd540916722e45f2ec
johnalexsagwe <johnsagwe@gmail.com>
2023-10-05 12:18:14 -0700

    hopefully final commit 1

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,12 +4,10 @@
 def magic_calculation(a, b):
     """Match bytecode provided by ALX SE School."""
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-
     else:
         return(sub(a, b))
