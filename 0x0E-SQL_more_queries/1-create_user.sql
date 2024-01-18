1fbff5fb73585ce107246860476c829feb398434
johnalexsagwe <johnsagwe@gmail>
2024-01-18 10:35:49 -0800

    Implement SQL queries to manage MySQL users, create databases and tables, perform data manipulation, and retrieve information about TV shows, genres, and their relationships as per specified tasks in the 0x0E-SQL_more_queries project.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates a user in mysql serve with all privileges
+-- if the user already exists do not create
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- lists all the genres of dexter
+SELECT tv_genres.name AS name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title="Dexter"
+ORDER BY name ASC;
